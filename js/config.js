c710ee296138a9cef25cb592a2c19429dd783905
Anastasiia Hevchuk <39774581+Anastasiia-Hevchuk@users.noreply.github.com>
2024-12-09 18:13:46 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-north-1_lF016N11l', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5dc1pdknllr7mnd37pbc15d79d', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-north-1_gk5ibwm4Q', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '34gt1uqn31batv6c2v072fc2pg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
